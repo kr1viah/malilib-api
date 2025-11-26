0ffe253d2a6425c971a38663399af561eaddb3af
kr1viah <kravpolet@gmail.com>
2025-11-26 15:56:44 +0100

    Init

---
+++ src/main/java/kr1v/malilibApi/MalilibApi.java
@@ -0,0 +1,10 @@
+package kr1v.malilibApi;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MalilibApi implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/kr1v/malilibApi/client/MalilibApiClient.java
@@ -0,0 +1,10 @@
+package kr1v.malilibApi.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MalilibApiClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
