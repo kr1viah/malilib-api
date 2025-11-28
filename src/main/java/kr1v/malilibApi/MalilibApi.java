135318ed2b8c7117bd5b5b4e6f0ad34d33a76b5e
kr1viah_ <kravpolet@gmail.com>
2025-11-28 19:52:33 +0100

    Don't save before loading

--- src/main/java/kr1v/malilibApi/MalilibApi.java
+++ src/main/java/kr1v/malilibApi/MalilibApi.java
@@ -40,7 +40,6 @@ public class MalilibApi {
         });
 
         ClientLifecycleEvents.CLIENT_STOPPING.register(client -> configHandler.save());
-        configHandler.onConfigsChanged();
     }
 
     public static void init() {
