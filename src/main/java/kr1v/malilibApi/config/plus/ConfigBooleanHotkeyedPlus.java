ce86a85c6a79dacd114cc21173e2c72409cfa0ee
kr1viah <kravpolet@gmail.com>
2026-01-09 17:45:57 +0100

    Separate ConfigBooleanPlus and ConfigBooleanHotkeyedPlus

---
+++ src/main/java/kr1v/malilibApi/config/plus/ConfigBooleanHotkeyedPlus.java
@@ -0,0 +1,77 @@
+package kr1v.malilibApi.config.plus;
+
+import fi.dy.masa.malilib.config.options.ConfigBoolean;
+import fi.dy.masa.malilib.config.options.ConfigBooleanHotkeyed;
+import fi.dy.masa.malilib.hotkeys.IHotkeyCallback;
+import fi.dy.masa.malilib.hotkeys.KeybindSettings;
+import fi.dy.masa.malilib.interfaces.IValueChangeCallback;
+import kr1v.malilibApi.InternalMalilibApi;
+
+public class ConfigBooleanHotkeyedPlus extends ConfigBooleanHotkeyed implements Plus {
+    {
+        getKeybind().setCallback((keyAction, keybind) -> {
+            setBooleanValue(!getBooleanValue());
+            return true;
+        });
+    }
+
+    public ConfigBooleanHotkeyedPlus(String name) {
+        this(name, InternalMalilibApi.getDefaultEnabled(), "", "");
+    }
+
+    public ConfigBooleanHotkeyedPlus(String name, boolean defaultValue) {
+        this(name, defaultValue, "", "");
+    }
+
+    public ConfigBooleanHotkeyedPlus(String name, boolean defaultValue, String defaultHotkey) {
+        this(name, defaultValue, defaultHotkey, "");
+    }
+
+    public ConfigBooleanHotkeyedPlus(String name, boolean defaultValue, String defaultHotkey, String comment) {
+        super(name, defaultValue, defaultHotkey, comment.isEmpty() ? " " : comment);
+    }
+
+    public ConfigBooleanHotkeyedPlus(String name, IHotkeyCallback callback) {
+        this(name, InternalMalilibApi.getDefaultEnabled(), "", "");
+        getKeybind().setCallback(callback);
+    }
+
+    public ConfigBooleanHotkeyedPlus(String name, boolean defaultValue, IHotkeyCallback callback) {
+        this(name, defaultValue, "", "");
+        getKeybind().setCallback(callback);
+    }
+
+    public ConfigBooleanHotkeyedPlus(String name, boolean defaultValue, String defaultHotkey, IHotkeyCallback callback) {
+        this(name, defaultValue, defaultHotkey, "");
+        getKeybind().setCallback(callback);
+    }
+
+    public ConfigBooleanHotkeyedPlus(String name, boolean defaultValue, String defaultHotkey, String comment, IHotkeyCallback callback) {
+        super(name, defaultValue, defaultHotkey, comment.isEmpty() ? " " : comment);
+        getKeybind().setCallback(callback);
+    }
+
+    public ConfigBooleanHotkeyedPlus(String name, boolean defaultValue, String defaultHotkey, KeybindSettings settings, String comment, String prettyName, String translatedName) {
+        super(name, defaultValue, defaultHotkey, settings, comment.isEmpty() ? " " : comment, prettyName, translatedName);
+    }
+
+    public ConfigBooleanHotkeyedPlus(String name, IValueChangeCallback<ConfigBoolean> callback) {
+        this(name, InternalMalilibApi.getDefaultEnabled(), "", "");
+        setValueChangeCallback(callback);
+    }
+
+    public ConfigBooleanHotkeyedPlus(String name, boolean defaultValue, IValueChangeCallback<ConfigBoolean> callback) {
+        this(name, defaultValue, "", "");
+        setValueChangeCallback(callback);
+    }
+
+    public ConfigBooleanHotkeyedPlus(String name, boolean defaultValue, String defaultHotkey, IValueChangeCallback<ConfigBoolean> callback) {
+        this(name, defaultValue, defaultHotkey, "");
+        setValueChangeCallback(callback);
+    }
+
+    public ConfigBooleanHotkeyedPlus(String name, boolean defaultValue, String defaultHotkey, String comment, IValueChangeCallback<ConfigBoolean> callback) {
+        super(name, defaultValue, defaultHotkey, comment.isEmpty() ? " " : comment);
+        setValueChangeCallback(callback);
+    }
+}
--- src/main/java/kr1v/malilibApi/config/plus/ConfigBooleanPlus.java
+++ src/main/java/kr1v/malilibApi/config/plus/ConfigBooleanPlus.java
@@ -1,77 +1,12 @@
 package kr1v.malilibApi.config.plus;
 
 import fi.dy.masa.malilib.config.options.ConfigBoolean;
-import fi.dy.masa.malilib.config.options.ConfigBooleanHotkeyed;
-import fi.dy.masa.malilib.hotkeys.IHotkeyCallback;
-import fi.dy.masa.malilib.hotkeys.KeybindSettings;
-import fi.dy.masa.malilib.interfaces.IValueChangeCallback;
 import kr1v.malilibApi.InternalMalilibApi;
 
-public class ConfigBooleanPlus extends ConfigBooleanHotkeyed implements Plus {
-    {
-        getKeybind().setCallback((keyAction, keybind) -> {
-            setBooleanValue(!getBooleanValue());
-            return true;
-        });
-    }
-
-    public ConfigBooleanPlus(String name) {
-        this(name, InternalMalilibApi.getDefaultEnabled(), "", "");
-    }
-
-    public ConfigBooleanPlus(String name, boolean defaultValue) {
-        this(name, defaultValue, "", "");
-    }
-
-    public ConfigBooleanPlus(String name, boolean defaultValue, String defaultHotkey) {
-        this(name, defaultValue, defaultHotkey, "");
-    }
-
-    public ConfigBooleanPlus(String name, boolean defaultValue, String defaultHotkey, String comment) {
-        super(name, defaultValue, defaultHotkey, comment.isEmpty() ? " " : comment);
-    }
-
-    public ConfigBooleanPlus(String name, IHotkeyCallback callback) {
-        this(name, InternalMalilibApi.getDefaultEnabled(), "", "");
-        getKeybind().setCallback(callback);
-    }
-
-    public ConfigBooleanPlus(String name, boolean defaultValue, IHotkeyCallback callback) {
-        this(name, defaultValue, "", "");
-        getKeybind().setCallback(callback);
-    }
-
-    public ConfigBooleanPlus(String name, boolean defaultValue, String defaultHotkey, IHotkeyCallback callback) {
-        this(name, defaultValue, defaultHotkey, "");
-        getKeybind().setCallback(callback);
-    }
-
-    public ConfigBooleanPlus(String name, boolean defaultValue, String defaultHotkey, String comment, IHotkeyCallback callback) {
-        super(name, defaultValue, defaultHotkey, comment.isEmpty() ? " " : comment);
-        getKeybind().setCallback(callback);
-    }
-
-    public ConfigBooleanPlus(String name, boolean defaultValue, String defaultHotkey, KeybindSettings settings, String comment, String prettyName, String translatedName) {
-        super(name, defaultValue, defaultHotkey, settings, comment.isEmpty() ? " " : comment, prettyName, translatedName);
-    }
-
-    public ConfigBooleanPlus(String name, IValueChangeCallback<ConfigBoolean> callback) {
-        this(name, InternalMalilibApi.getDefaultEnabled(), "", "");
-        setValueChangeCallback(callback);
-    }
-
-    public ConfigBooleanPlus(String name, boolean defaultValue, IValueChangeCallback<ConfigBoolean> callback) {
-        this(name, defaultValue, "", "");
-        setValueChangeCallback(callback);
-    }
-
-    public ConfigBooleanPlus(String name, boolean defaultValue, String defaultHotkey, IValueChangeCallback<ConfigBoolean> callback) {
-        this(name, defaultValue, defaultHotkey, "");
-        setValueChangeCallback(callback);
-    }
-
-    public ConfigBooleanPlus(String name, boolean defaultValue, String defaultHotkey, String comment, IValueChangeCallback<ConfigBoolean> callback) {
-        super(name, defaultValue, defaultHotkey, comment.isEmpty() ? " " : comment);
-        setValueChangeCallback(callback);
-    }
+// you are encouraged to use ConfigBooleanPlus instead, because it has a hotkey for toggling it.
+// use this if it doesn't make sense to have a hotkey to toggle it.
+public class ConfigBooleanPlus extends ConfigBoolean implements Plus {
+	public ConfigBooleanPlus(String name) {
+		super(name, InternalMalilibApi.getDefaultEnabled(), " ");
+	}
 }
