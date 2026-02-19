dce5c6d07ad22e957292593ab8f8af72acacf0a4
kr1viah <kravpolet@gmail.com>
2026-02-19 15:10:21 +0100

    More settings for your convenience

--- src/main/java/kr1v/malilibApi/config/plus/ConfigBooleanHotkeyedPlus.java
+++ src/main/java/kr1v/malilibApi/config/plus/ConfigBooleanHotkeyedPlus.java
@@ -3,6 +3,7 @@ package kr1v.malilibApi.config.plus;
 import fi.dy.masa.malilib.config.options.ConfigBoolean;
 import fi.dy.masa.malilib.config.options.ConfigBooleanHotkeyed;
 import fi.dy.masa.malilib.hotkeys.IHotkeyCallback;
+import fi.dy.masa.malilib.hotkeys.KeyAction;
 import fi.dy.masa.malilib.hotkeys.KeybindSettings;
 import kr1v.malilibApi.InternalMalilibApi;
 
@@ -35,5 +36,109 @@ public class ConfigBooleanHotkeyedPlus extends ConfigBooleanHotkeyed implements 
 	public ConfigBooleanHotkeyedPlus setPressCallback(IHotkeyCallback callback) {
 		this.getKeybind().setCallback(callback);
 		return this;
+	}
+	public ConfigBooleanHotkeyedPlus setContext(KeybindSettings.Context context) {
+		KeybindSettings originalSettings = this.getKeybind().getSettings();
+		return this.setSettings(
+				KeybindSettings.create(
+						context,
+						originalSettings.getActivateOn(),
+						originalSettings.getAllowExtraKeys(),
+						originalSettings.isOrderSensitive(),
+						originalSettings.isExclusive(),
+						originalSettings.shouldCancel(),
+						originalSettings.getAllowEmpty()
+				)
+		);
+	}
+
+	public ConfigBooleanHotkeyedPlus setActivateOn(KeyAction activateOn) {
+		KeybindSettings originalSettings = this.getKeybind().getSettings();
+		return this.setSettings(
+				KeybindSettings.create(
+						originalSettings.getContext(),
+						activateOn,
+						originalSettings.getAllowExtraKeys(),
+						originalSettings.isOrderSensitive(),
+						originalSettings.isExclusive(),
+						originalSettings.shouldCancel(),
+						originalSettings.getAllowEmpty()
+				)
+		);
+	}
+
+	public ConfigBooleanHotkeyedPlus setAllowExtraKeys(boolean allowExtraKeys) {
+		KeybindSettings originalSettings = this.getKeybind().getSettings();
+		return this.setSettings(
+				KeybindSettings.create(
+						originalSettings.getContext(),
+						originalSettings.getActivateOn(),
+						allowExtraKeys,
+						originalSettings.isOrderSensitive(),
+						originalSettings.isExclusive(),
+						originalSettings.shouldCancel(),
+						originalSettings.getAllowEmpty()
+				)
+		);
+	}
+
+	public ConfigBooleanHotkeyedPlus setOrderSensitive(boolean orderSensitive) {
+		KeybindSettings originalSettings = this.getKeybind().getSettings();
+		return this.setSettings(
+				KeybindSettings.create(
+						originalSettings.getContext(),
+						originalSettings.getActivateOn(),
+						originalSettings.getAllowExtraKeys(),
+						orderSensitive,
+						originalSettings.isExclusive(),
+						originalSettings.shouldCancel(),
+						originalSettings.getAllowEmpty()
+				)
+		);
+	}
+
+	public ConfigBooleanHotkeyedPlus setExclusive(boolean exclusive) {
+		KeybindSettings originalSettings = this.getKeybind().getSettings();
+		return this.setSettings(
+				KeybindSettings.create(
+						originalSettings.getContext(),
+						originalSettings.getActivateOn(),
+						originalSettings.getAllowExtraKeys(),
+						originalSettings.isOrderSensitive(),
+						exclusive,
+						originalSettings.shouldCancel(),
+						originalSettings.getAllowEmpty()
+				)
+		);
+	}
+
+	public ConfigBooleanHotkeyedPlus setShouldCancel(boolean shouldCancel) {
+		KeybindSettings originalSettings = this.getKeybind().getSettings();
+		return this.setSettings(
+				KeybindSettings.create(
+						originalSettings.getContext(),
+						originalSettings.getActivateOn(),
+						originalSettings.getAllowExtraKeys(),
+						originalSettings.isOrderSensitive(),
+						originalSettings.isExclusive(),
+						shouldCancel,
+						originalSettings.getAllowEmpty()
+				)
+		);
+	}
+
+	public ConfigBooleanHotkeyedPlus setAllowEmpty(boolean allowEmpty) {
+		KeybindSettings originalSettings = this.getKeybind().getSettings();
+		return this.setSettings(
+				KeybindSettings.create(
+						originalSettings.getContext(),
+						originalSettings.getActivateOn(),
+						originalSettings.getAllowExtraKeys(),
+						originalSettings.isOrderSensitive(),
+						originalSettings.isExclusive(),
+						originalSettings.shouldCancel(),
+						allowEmpty
+				)
+		);
 	}
 }
--- src/main/java/kr1v/malilibApi/config/plus/ConfigHotkeyPlus.java
+++ src/main/java/kr1v/malilibApi/config/plus/ConfigHotkeyPlus.java
@@ -2,6 +2,7 @@ package kr1v.malilibApi.config.plus;
 
 import fi.dy.masa.malilib.config.options.ConfigHotkey;
 import fi.dy.masa.malilib.hotkeys.IHotkeyCallback;
+import fi.dy.masa.malilib.hotkeys.KeyAction;
 import fi.dy.masa.malilib.hotkeys.KeybindSettings;
 
 public class ConfigHotkeyPlus extends ConfigHotkey implements Plus<ConfigHotkey> {
@@ -33,5 +34,112 @@ public class ConfigHotkeyPlus extends ConfigHotkey implements Plus<ConfigHotkey>
 	public ConfigHotkeyPlus setPressCallback(IHotkeyCallback callback) {
 		this.getKeybind().setCallback(callback);
 		return this;
+	}
+
+	// lowk this sucks but whatever, it works
+
+	public ConfigHotkeyPlus setContext(KeybindSettings.Context context) {
+		KeybindSettings originalSettings = this.getKeybind().getSettings();
+		return this.setSettings(
+				KeybindSettings.create(
+						context,
+						originalSettings.getActivateOn(),
+						originalSettings.getAllowExtraKeys(),
+						originalSettings.isOrderSensitive(),
+						originalSettings.isExclusive(),
+						originalSettings.shouldCancel(),
+						originalSettings.getAllowEmpty()
+				)
+		);
+	}
+
+	public ConfigHotkeyPlus setActivateOn(KeyAction activateOn) {
+		KeybindSettings originalSettings = this.getKeybind().getSettings();
+		return this.setSettings(
+				KeybindSettings.create(
+						originalSettings.getContext(),
+						activateOn,
+						originalSettings.getAllowExtraKeys(),
+						originalSettings.isOrderSensitive(),
+						originalSettings.isExclusive(),
+						originalSettings.shouldCancel(),
+						originalSettings.getAllowEmpty()
+				)
+		);
+	}
+
+	public ConfigHotkeyPlus setAllowExtraKeys(boolean allowExtraKeys) {
+		KeybindSettings originalSettings = this.getKeybind().getSettings();
+		return this.setSettings(
+				KeybindSettings.create(
+						originalSettings.getContext(),
+						originalSettings.getActivateOn(),
+						allowExtraKeys,
+						originalSettings.isOrderSensitive(),
+						originalSettings.isExclusive(),
+						originalSettings.shouldCancel(),
+						originalSettings.getAllowEmpty()
+				)
+		);
+	}
+
+	public ConfigHotkeyPlus setOrderSensitive(boolean orderSensitive) {
+		KeybindSettings originalSettings = this.getKeybind().getSettings();
+		return this.setSettings(
+				KeybindSettings.create(
+						originalSettings.getContext(),
+						originalSettings.getActivateOn(),
+						originalSettings.getAllowExtraKeys(),
+						orderSensitive,
+						originalSettings.isExclusive(),
+						originalSettings.shouldCancel(),
+						originalSettings.getAllowEmpty()
+				)
+		);
+	}
+
+	public ConfigHotkeyPlus setExclusive(boolean exclusive) {
+		KeybindSettings originalSettings = this.getKeybind().getSettings();
+		return this.setSettings(
+				KeybindSettings.create(
+						originalSettings.getContext(),
+						originalSettings.getActivateOn(),
+						originalSettings.getAllowExtraKeys(),
+						originalSettings.isOrderSensitive(),
+						exclusive,
+						originalSettings.shouldCancel(),
+						originalSettings.getAllowEmpty()
+				)
+		);
+	}
+
+	public ConfigHotkeyPlus setShouldCancel(boolean shouldCancel) {
+		KeybindSettings originalSettings = this.getKeybind().getSettings();
+		return this.setSettings(
+				KeybindSettings.create(
+						originalSettings.getContext(),
+						originalSettings.getActivateOn(),
+						originalSettings.getAllowExtraKeys(),
+						originalSettings.isOrderSensitive(),
+						originalSettings.isExclusive(),
+						shouldCancel,
+						originalSettings.getAllowEmpty()
+				)
+		);
+	}
+
+	public ConfigHotkeyPlus setAllowEmpty(boolean allowEmpty) {
+		KeybindSettings originalSettings = this.getKeybind().getSettings();
+		return this.setSettings(
+				KeybindSettings.create(
+						originalSettings.getContext(),
+						originalSettings.getActivateOn(),
+						originalSettings.getAllowExtraKeys(),
+						originalSettings.isOrderSensitive(),
+						originalSettings.isExclusive(),
+						originalSettings.shouldCancel(),
+						allowEmpty
+				)
+		);
 	}
 }
